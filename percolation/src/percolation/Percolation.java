0fca1655895f5e167f7516f9b15b75d01b208198
Srinivasan <Srinivasan@Srinivasan>
2017-12-09 15:41:50 +0530

    89/100 code

--- percolation/src/percolation/Percolation.java
+++ percolation/src/percolation/Percolation.java
@@ -1,67 +1,71 @@
 package percolation;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
-	WeightedQuickUnionUF uf;
+	private WeightedQuickUnionUF uf;
 	private boolean[][] box;
-	private int top=0;
+	private int top = 0;
 	private int bottom;
 	private int size;
-	private int opensites=0;
-	   public Percolation(int n) {
-		   uf=new WeightedQuickUnionUF(n*n+2);
-		   size=n;
-		   box=new boolean[n][n];
-		   bottom=size*size+1;
-	   }
-	   public void open(int row, int col) {
-		   box[row-1][col-1]=true;
-		   if(row==1) {
-			   uf.union(col, top);
-		   }
-		   if(row==size) {
-			   uf.union(rowmajor(row, col), bottom);
-		   }
-		   if(row>1 && isOpen(row-1, col)) {
-			   uf.union(rowmajor(row, col), rowmajor(row-1, col));
-		   }
-		   if(row<size && isOpen(row+1, col)) {
-			   uf.union(rowmajor(row, col), rowmajor(row+1, col));
-		   }
-		   if(col>1 && isOpen(row, col-1)) {
-			   uf.union(rowmajor(row, col), rowmajor(row, col-1));
-		   }
-		   if(col<size && isOpen(row, col+1)) {
-			   uf.union(rowmajor(row, col), rowmajor(row, col+1));
-		   }
-	   }
-	   public boolean isOpen(int row, int col) {
-		   return box[row-1][col-1];
-	   }
-	   public boolean isFull(int row, int col) {
-		   if(row>0 && row<size && col<0 && col>size) {
-		   return uf.connected(top,rowmajor(row,col) );
-		   }else {
-			   throw new IndexOutOfBoundsException();
-		   }
-			   
-	   }
-	   public int numberOfOpenSites() {
+	private int opensites = 0;
+	public Percolation(int n) {
+		if (n <= 0){
+			throw new java.lang.IllegalArgumentException();
+		} else {
+		uf = new WeightedQuickUnionUF(n*n+2);
+		size = n;
+		box = new boolean[n][n];
+		bottom = size*size+1;
+		}
+	}
+	public void open(int row, int col) {
+		if(row > size || col > size || row <=0 || col <= 0){
+			throw new java.lang.IllegalArgumentException();
+		} else {
+		opensites++;
+		box[row-1][col-1] = true;
+		if (row == 1) {
+		   uf.union(helper(row, col), top);
+		}
+		if (row == size) {
+			uf.union(helper(row, col), bottom);
+		}
+		if (row > 1 && isOpen(row-1, col)) {
+		   uf.union(helper(row, col), helper(row-1, col));
+		}
+		if (row < size && isOpen(row+1, col)) {
+		   uf.union(helper(row, col), helper(row+1, col));
+		}
+		if (col > 1 && isOpen(row, col-1)) {
+		   uf.union(helper(row, col), helper(row, col-1));
+		}
+		if (col < size && isOpen(row, col+1)) {
+		   uf.union(helper(row, col), helper(row, col+1));
+		}
+		}
+	}
+	public boolean isOpen(int row, int col) {
+		if(row > size || col > size || row <=0 || col <= 0){
+			throw new java.lang.IllegalArgumentException();
+		} else {
+	   return box[row-1][col-1];
+		}
+	}
+	public boolean isFull(int row, int col) {
+	   if (row > 0 && row <= size && col >0 && col <= size) {
+	   return uf.connected(top , helper(row,col));
+	   } else { 
+		   throw new java.lang.IllegalArgumentException();
+	   }	   
+	}
+	public int numberOfOpenSites() {
 		   // number of open sites
-		   for(int i=0;i<size;i++) {
-			   for(int j=0;j<size;j++) {
-				   if(box[i][j]==true) {
-					   opensites++;
-				   }
-			   }
-		   }
 		   return opensites;
 	   }
-	   public boolean percolates() {
-		   
-		   return uf.connected(top, bottom);
-	   }
-	   private int rowmajor(int x, int y) {
-		   return size*(x-1)+y;
-	   }
+	public boolean percolates() {
+		return uf.connected(top, bottom);
+	}
+	private int helper(int x, int y) {
+		return size*(x-1)+(y);
+	}
 
 }
--- percolation/src/percolation/PercolationStats.java
+++ percolation/src/percolation/PercolationStats.java
@@ -3,50 +3,47 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 public class PercolationStats {
+	private static float az = 1.96f;
 	private int c;
-	private Percolation pr;
 	private double[] f;
 	public PercolationStats(int n, int trials) {
-		if(n<=0 || trials<=0) {
+		if (n <= 0 || trials <= 0) {
 			throw new IllegalArgumentException("n<=0 or trails<=0");
 		}
-		c=trials;
-		f=new double[c];
-		for(int i=0;i<c;i++) {
-			pr=new Percolation(n);
-			int open=0;
-			while (!pr.percolates()) {
-				int x=StdRandom.uniform(1, n+1);
-				int y=StdRandom.uniform(1, n+1);
-				if(!pr.isOpen(x,y)) {
-					pr.open(x, y);
+		c = trials;
+		f = new double[c]; 
+		for (int i = 0 ; i < c; i++) {
+			Percolation pr = new Percolation(n);
+			int open = 0;
+			while (!pr.percolates()) { 
+				int x = StdRandom.uniform(1 , n+1); 
+				int y = StdRandom.uniform(1 , n+1);
+				if(!pr.isOpen(x , y)) {
+					pr.open(x , y);
 					open++;
 				}
 			}
-			double f1=(double) open/(n*n);
-			f[i]=f1;
+			double f1 = (double) open/(n*n);
+			f[i] = f1;
 		}
 	}
-	   public double mean() {
-		   // sample mean of percolation threshold
-		   return StdStats.mean(f);
-	   }
-	   public double stddev() {
-		   // sample standard deviation of percolation threshold
-		   return StdStats.stddev(f);
-	   }
-	   public double confidenceLo() {
-		   // low  endpoint of 95% confidence interval
-		   return mean()-((1.96*stddev())/Math.sqrt(c));
-	   }
-	   public double confidenceHi() {
-		   return mean()+((1.96*stddev())/Math.sqrt(c));
-	   }
-	   public static void main(String[] args) {
-		int n=Integer.parseInt(args[0]);
-		int t=Integer.parseInt(args[1]);
-		PercolationStats p=new PercolationStats(n, t);
-		String cc=p.confidenceLo()+", "+p.confidenceHi();
+	public double mean() {
+		return StdStats.mean(f);
+	}
+	public double stddev() {
+		return StdStats.stddev(f);
+	}
+	public double confidenceLo() {
+		return mean()-((az*stddev())/Math.sqrt(c));
+	}
+	public double confidenceHi() {
+		return mean()+((az*stddev())/Math.sqrt(c));
+	}
+	public static void main(String[] args) {
+		int n = Integer.parseInt(args[0]);
+		int t = Integer.parseInt(args[1]);
+		PercolationStats p = new PercolationStats(n, t);
+		String cc = p.confidenceLo()+", "+p.confidenceHi();
 		StdOut.println("mean                    ="+p.mean());
 		StdOut.println("stddiv                  ="+p.stddev());
 		StdOut.println("95% confidence interval = "+cc);
